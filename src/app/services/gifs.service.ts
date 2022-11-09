e5ac240ad6be1bd0306881f0ab511fa1b5273675
HERNAN21 <hernanrojasutani@gmail.com>
2022-11-09 09:55:09 -0500

    LocalStorege cargar imagen automaticamente

--- src/app/services/gifs.service.ts
+++ src/app/services/gifs.service.ts
@@ -9,15 +9,17 @@ export class GifsService {
 
   // private apiKey: string= 'OwK4TGIKcSs7cutUEc3hZl43Hgppb14P';
 
+  private _historial: string[] = [];
+
+  public resultados: Gif[] = [];
+
   constructor(
     private http: HttpClient
   ) { 
     this._historial=JSON.parse(localStorage.getItem('historial')!) || [];
+    this.resultados=JSON.parse(localStorage.getItem('resultados')!) || [];
   }
 
-  private _historial: string[] = [];
-
-  public resultados: Gif[] = [];
 
   get historial(){
     return [...this._historial];
@@ -36,6 +38,7 @@ export class GifsService {
     this.http.get<SearchGifsResponse>(`https://api.giphy.com/v1/gifs/search?api_key=OwK4TGIKcSs7cutUEc3hZl43Hgppb14P&q=${query}&limit=10`)
     .subscribe((resp: SearchGifsResponse)=>{
       this.resultados=resp.data;
+      localStorage.setItem('resultados', JSON.stringify(this.resultados));
       console.log(resp.data);
     })
 
